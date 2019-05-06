88294c86068f17d007092bd41ea4e061e761a3b0
Vitor Perles <vitorhugoperles@gmail.com>
2019-05-06 15:39:39 -0300

    Sistema de Validação Incluida, removido módulos desnecessários

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,42 +1,72 @@
 import * as React from 'react';
-import * as ReactDom from 'react-dom'
+import * as ReactDom from 'react-dom';
 import TextField from 'material-ui/TextField';
 
 
-class LoginButton extends React.Component{
-    render(){
-        return(
-            <button type = {this.props.type} className = {"LoginButton"}>{this.props.text}</button>
-        );
+class Validator{
+    public isEmail(text:string): Boolean{
+        const regex = /\w+@\w+.com$/gm;
+        return regex.test(text);
+    }
+    public isPassword(text:string): Boolean{
+        const regex = /^(?=.*\d)(?=.*[a-zA-Z]).{7,}$/gm;
+        return regex.test(text);
     }
 }
 
-class LoginTitle extends React.Component{
-    render(){
-        return(
-            <h1 className = {"LoginTitle"}>{this.props.text}</h1>
-        );
-    }
-}
-
-class Form extends React.Component{
-    render(){
-        return(
-            <input type={this.props.type} className = {"Form"} placeholder={this.props.placeholder} />
-        );
-    }
-}
 export class Login extends React.Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {
+          email: null,
+          password: null,
+          isEmailValid: false,
+          isPasswordValid: false,
+          submitted: false,
+        };
+      }   
+
+    handleChange = (event) => {
+        event.preventDefault();
+        const { name, value } = event.target;
+        let validation = new Validator();
+        switch (name) {
+          case 'email': 
+          this.setState({
+            isEmailValid: validation.isEmail(value),
+            }) ;
+            break;
+          case 'password': 
+          this.setState({
+            isPasswordValid: validation.isPassword(value),
+            }) ;
+            break;
+          default:
+            break;
+        }
+      }
+
+      handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            submitted: true,
+        }) ;
+        console.log(this.state);
+      }
+
+      
     render(){
+        const {isEmailValid,isPasswordValid,submitted} = this.state;
         return(
-            <div class ="Login">
-            
-            <form method="post">
-              <LoginTitle text = "Bem vindo à TaqTile"/>
-              <Form type="text" placeholder="Login"/>
-              <Form type="password" name="p" placeholder="Password"/>
-              <LoginButton type = {"submit"} text = {"Login"}/>
+            <div className ="Login">
+            <form method="post" onSubmit = {this.handleSubmit} noValidate>
+              <h1 className = {"LoginTitle"}>Bem vindo à TaqTile</h1>
+              <input type={"text"} className = {"FormInput"} name = {"email"} placeholder={"Login"} onChange={this.handleChange} required />
+              {(submitted && !isEmailValid) && <span className='error'>"Email Inválido "</span>}
+              <input type={"password"} className = {"FormInput"} name = {"password" } placeholder={"Senha"} onChange={this.handleChange} required />
+              {(submitted && !isPasswordValid) && <span className='error'>"Sua senha deve conter no mínimo um digito e um caractere alfanumérico, além de ser maior que 7 digitos"</span>}
+              <button type = {"submit"} className = {"LoginButton"}>Fazer Login</button>
               </form>
           </div>
         );
