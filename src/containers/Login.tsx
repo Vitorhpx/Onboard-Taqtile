3b4c53e47fbb3d4301b27f0718cd406ab08f26e6
Taqtile <taqtile@Brooklyn.local>
2019-05-07 14:08:44 -0300

    Refatoração por meio da extração do email e password fields

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,51 +1,23 @@
 import * as React from 'react';
-import * as ReactDom from 'react-dom';
 import TextField from 'material-ui/TextField';
+import { Validator } from '../utils/validator';
+import { EmailField } from '../components/email-field';
+import { PasswordField } from '../components/password-field';
 
-
-class Validator{
-    public isEmail(text:string): Boolean{
-        const regex = /\w+@\w+.com$/gm;
-        return regex.test(text);
-    }
-    public isPassword(text:string): Boolean{
-        const regex = /^(?=.*\d)(?=.*[a-zA-Z]).{7,}$/gm;
-        return regex.test(text);
-    }
+interface LoginProps {
 }
 
-export class Login extends React.Component{
+interface LoginState {
+  submitted: boolean;
+}
 
+export class Login extends React.Component<LoginProps, LoginState> {
     constructor(props) {
         super(props);
         this.state = {
-          email: null,
-          password: null,
-          isEmailValid: false,
-          isPasswordValid: false,
           submitted: false,
         };
       }   
-
-    handleChange = (event) => {
-        event.preventDefault();
-        const { name, value } = event.target;
-        let validation = new Validator();
-        switch (name) {
-          case 'email': 
-          this.setState({
-            isEmailValid: validation.isEmail(value),
-            }) ;
-            break;
-          case 'password': 
-          this.setState({
-            isPasswordValid: validation.isPassword(value),
-            }) ;
-            break;
-          default:
-            break;
-        }
-      }
 
       handleSubmit = (event) => {
         event.preventDefault();
@@ -54,19 +26,17 @@ export class Login extends React.Component{
         }) ;
         console.log(this.state);
       }
-
       
     render(){
-        const {isEmailValid,isPasswordValid,submitted} = this.state;
+        const {submitted} = this.state;
         return(
             <form method = "post" onSubmit = {this.handleSubmit} noValidate className = "Login">
               <h1 className = "LoginTitle">Bem vindo à TaqTile</h1>
-              <input type = "text" className = "FormInput" name = "email" placeholder = "Login" onChange = {this.handleChange} required />
-              {(submitted && !isEmailValid) && <span className = 'Error'>Email Inválido</span>}
-              <input type = "password" className = "FormInput" name = "password" placeholder = "Senha" onChange = {this.handleChange} required />
-              {(submitted && !isPasswordValid) && <span className = 'Error'>Deve conter pelo menos 7 caracteres, com 1 alfanumérico e 1 dígito</span>}
+              <EmailField canShowError={submitted}/>
+              <PasswordField canShowError={submitted}/>
               <button type = "submit" className = "LoginButton">Fazer Login</button>
-              </form>
+            </form>
         );
     };
 };
+
