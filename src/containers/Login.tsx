55bf40d983e2b503c5eddbdbdf001041ee6806e4
Vitor Perles <vitorhugoperles@gmail.com>
2019-05-07 23:01:21 -0300

    Token salvo no local Storage, mensagem inicial de erro

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -15,6 +15,7 @@ interface LoginState {
   password: string;
   isEmailValid: boolean;
   isPasswordValid: boolean;
+  errorMessage: string;
 }
 
 const LOGIN_MUTATION = gql`
@@ -33,19 +34,20 @@ export class Login extends React.Component<LoginProps, LoginState> {
           email: "",
           password:"",
           isEmailValid: false,
-          isPasswordValid: false
+          isPasswordValid: false,
+          errorMessage: ""
         };
       }
 
-      handleSubmit = (mutateFunction : Function, event) => {
+      handleSubmit = async (mutateFunction : Function, event) => {
         event.preventDefault();
         const { name, value } = event.target;
         this.setState({
             submitted: true,
         }) ;
         console.log(this.state);
-        if(this.state.isEmailValid && this.state.isPasswordValid){
-          mutateFunction();
+        if(this.state.isEmailValid /*&& this.state.isPasswordValid*/){
+          await mutateFunction();
         }
       }
 
@@ -73,22 +75,29 @@ export class Login extends React.Component<LoginProps, LoginState> {
         }) ;
       }
 
+      _confirm = async data => {
+        const {token} = data.Login;
+        localStorage.setItem('token', token);
+      }
+
     render(){
         const {submitted} = this.state;
         return(
           <Mutation
               mutation = {LOGIN_MUTATION}
-              variables = {{"email":this.state.email,"password":this.state.password}}>
+              variables = {{"email":this.state.email,"password":this.state.password}}
+              onCompleted={errors => this._confirm(errors)}>
               {
-                mutation => (
+                (mutation, { loading, error }) => (
             <form method = "post" onSubmit = {(event) => {this.handleSubmit(mutation, event)}} noValidate className = "Login">
               <h1 className = "LoginTitle">Bem vindo à TaqTile</h1>
               <EmailField canShowError={submitted} setEmail={this.handleSetEmail} setValid = {this.handleEmailSetValid}/>
               <PasswordField canShowError={submitted} setPassword={this.handlePasswordChange} setValid = {this.handlePasswordSetValid}/>
               <button type = "submit" className = "LoginButton">Fazer Login</button>
+              {loading && <p>Loading...</p>}
+              {error && <p>Erro!</p>}
             </form>
-            )
-          }
+                )}
           </Mutation>
         );
     }
