a4a74336fd18cfbe6451411995d131ddd3f9ac98
Taqtile <taqtile@Brooklyn.local>
2019-05-09 10:09:37 -0300

    Padronização da identação

--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react';
-import {gql} from "apollo-boost";
-import { Query, QueryResult } from 'react-apollo';
-import { Layout } from '../layout';
-import { string } from 'prop-types';
-import {UserCard} from '../containers/user-card'
+import * as React from "react"
+import { gql } from "apollo-boost"
+import { Query, QueryResult } from "react-apollo"
+import { Layout } from "../layout"
+import { string } from "prop-types"
+import { UserCard } from "../containers/user-card"
 
 const USERS_QUERY = gql`
-  query getUsers{
-    Users{
+  query getUsers {
+    Users {
       nodes {
         name
         email
@@ -16,44 +16,37 @@ const USERS_QUERY = gql`
   }
 `
 type User = {
-  name: string;
-  email: string;
-};
-type Users ={
-  nodes: User[];
+  name: string
+  email: string
+}
+type Users = {
+  nodes: User[]
 }
 
 type Response = {
   Users: Users
 }
 
+export default class UserList extends React.Component<any, any> {
+  constructor(props) {
+    super(props)
+  }
 
-interface UserListProps {
+  render() {
+    return (
+      <Layout>
+        <Query query={USERS_QUERY}>
+          {(response: QueryResult<Response>) => {
+            if (response.loading) return "Loading..."
+            if (response.error) return `Error! ${response.error.message}`
+            return response.data.Users.nodes.map(function(user, index) {
+              return (
+                <UserCard email={user.email} username={user.name} key={index} />
+              )
+            })
+          }}
+        </Query>
+      </Layout>
+    )
+  }
 }
-
-interface UserListState {
-}
-
-export default class UserList extends React.Component<UserListProps, UserListState> {
-    constructor(props) {
-        super(props);
-      }
-
-    render(){
-      return(
-        <Layout>
-          <Query query = {USERS_QUERY}>{(response: QueryResult<Response>) => {
-            if (response.loading) return "Loading...";
-            if (response.error) return `Error! ${response.error.message}`;
-            return (
-              response.data.Users.nodes.map(function (user, index) {
-                return <UserCard email = {user.email} username = {user.name} key = {index}></UserCard>
-              })
-            )
-            }}
-          </Query>
-        </Layout>
-
-      );
-    }
-};
