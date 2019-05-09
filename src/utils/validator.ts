6b6de67ecd049c8a25304af26f286da80955c05c
Vitor Perles <vitorhugoperles@gmail.com>
2019-05-09 13:37:28 -0300

    Updated Regex

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -8,23 +8,23 @@ export class Validator{
         return regex.test(text);
     }
     static isId(text:string): boolean{
-        const regex = /^(?=.*\d)(?=.*[a-zA-Z]).{7,}$/gm;
+        const regex = /^[0-9]+$/gm;
         return regex.test(text);
     }
     static isName(text:string): boolean{
-        const regex = /^(?=.*\d)(?=.*[a-zA-Z]).{7,}$/gm;
+        const regex = /^[A-Z][a-z]+[\s,][A-Z][a-z]+$/gm;
         return regex.test(text);
     }
     static isCpf(text:string): boolean{
-      const regex = /^(?=.*\d)(?=.*[a-zA-Z]).{7,}$/gm;
+      const regex = /^[0-9]{11}$/gm;
       return regex.test(text);
     }
     static isBirthDate(text:string): boolean{
-      const regex = /^(?=.*\d)(?=.*[a-zA-Z]).{7,}$/gm;
+      const regex = /^[0-2][0-9]{3}[-](0[1-9]|1[0-2])[-](0[1-9]|[1-2][0-9])$/gm;
       return regex.test(text);
     }
     static isRole(text:string): boolean{
-      const regex = /^(?=.*\d)(?=.*[a-zA-Z]).{7,}$/gm;
+      const regex = /^(user|admin)$/gm;
       return regex.test(text);
     }
 }
