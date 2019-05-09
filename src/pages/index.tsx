fc68483223a08d5d90b4f192b08534d2096c546c
Taqtile <taqtile@Brooklyn.local>
2019-05-09 10:01:22 -0300

    Updated architecture to use Gatsby layouts, Listing users in the UserList Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,18 @@
+import * as React from 'react';
+import { Layout } from '../layout';
+import { Link } from 'gatsby';
 
-import React from 'react';
-import {Hello}  from '../components/Hello';
-import { Login } from '../pages/Login';
-import { UserList } from '../pages/UserList';
-import "../styles/styles.css";
-import { ApolloProvider } from "react-apollo";
-import { ApolloClient, ApolloLink, InMemoryCache, HttpLink } from 'apollo-boost';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import {AUTH_TOKEN} from "../constants"
-
-const httpLink = new HttpLink({ uri: "https://tq-template-server-sample.herokuapp.com/graphql" });
-
-const authLink = new ApolloLink((operation, forward) => {
-
-  const token = localStorage.getItem(AUTH_TOKEN);
-
-  if(token != null){
-    operation.setContext({
-
-      headers: {
-        authorization: token
+export default class Home extends React.Component<any, any> {
+    constructor(props) {
+        super(props);
       }
-    });
-  }
 
-  return forward(operation);
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink), // Chain it with the HttpLink
-  cache: new InMemoryCache()
-});
-
-export default () =>
-              <Router>
-                <ApolloProvider client={client}>
-                <Route path="/" exact component={Login} />
-                <Route path="/UserList/" component={UserList} />
-                </ApolloProvider>
-              </Router>
-
-
+    render(){
+      return(
+        <Layout>
+          <h1> Home </h1>
+          <Link to={'/Login'}> Login </Link>
+        </Layout>
+      );
+    }
+};
